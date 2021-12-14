fb977d6bf1e7ba2a533290e1da79228d1a4a1433
JamesMB209 <jmb209@gmail.com>
2021-12-14 18:32:05 +0800

    directory setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
+const basicAuth = require("express-handlebars");
+// const authChallenger = require("./AutherChallenger");
+// const knewConfig = require("./knexfile").development;
+// const knex = require("kenx")(knexConfig);
