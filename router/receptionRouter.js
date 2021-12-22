05e05ec09645b3be4cfece58804f6fbf48f1c166
Peram Kharel <peram1769@gmail.com>
2021-12-22 16:12:34 +0800

    finishedListingInAdmin

--- router/receptionRouter.js
+++ router/receptionRouter.js
@@ -10,13 +10,23 @@ class Reception {
    router() {
     let router = express.Router();
     router.get("/", this.get.bind(this));
-    // router.post("/", this.somefucntion.bind(this));
+    router.post("/", this.put.bind(this));
     return router;
    }
    
-   get(req, res) {
-    res.render("admin")
-   }
+    get(req, res) {
+            res.render("admin", {
+                doctor: this.doctors,
+            })
+    }
+
+    put(req, res) {
+        if(Array.isArray(this.doctors[0].queue)) {
+            if(this.doctors[0].queue.length === 0) {
+                console.log(`No one in que`)
+            } 
+        }
+    }
 }
 
 
