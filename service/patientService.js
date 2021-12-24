882eba11b26f46990d27aecb6258ed4b714c2be8
Pris <prischenghk@gmail.com>
2021-12-24 16:28:55 +0800

    pris patient age

--- service/patientService.js
+++ service/patientService.js
@@ -1,24 +1,24 @@
 class Patient {
 	constructor(patient) {
-	this.fName = patient.fName;
-	this.lName = patient.lName;
-	this.temperature = patient.temperature 
-	this.assignedDoctor = patient.doctor;
-	this.hkid = patient.hkid;
-	this.dob = patient.dob;
-	this.gender = patient.gender;
+		
+		this.fName = patient.fName;
+		this.lName = patient.lName;
+		this.temperature = patient.temperature 
+		this.assignedDoctor = patient.doctor;
+		this.hkid = patient.hkid;
+		this.dob = patient.dob;
+		this.gender = patient.gender;
 
-	this.fullName = `${this.fName} ${this.lName}`;
-	this.url = `${this.fName}_${this.lName}`.toLowerCase();
-	this.queuePosition;
-	this.arrived = new Date;
-	this.departed = "";
-	//this.visitReason = patient.visitReason;
-	//this.history = patient.history;
+		this.fullName = `${this.fName} ${this.lName}`;
+		this.url = `${this.fName}_${this.lName}`.toLowerCase();
+		this.queuePosition;
+		this.departed = "";
+	
+		this.arrived = new Date();
+		this.visitReason = patient.visitReason;
 	}
 	
 	//index in queue (can be some logic in here to calculate and return average waiting time)
 }
-
 
 module.exports = Patient;
